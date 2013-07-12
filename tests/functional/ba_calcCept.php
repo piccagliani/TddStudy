0d63dd00a4578fdd5e7bf4da2bdfcea23238805f
piccagliani <piccagliani.dev@gmail.com>
2013-07-12 13:11:32 +0900

    [TDDBC-Yokohama] Done Task 2.
    - 選手の打率を表示用に整形したものを出力できること。
    - 打率の整数部は消去して表示すること。(例: 0.333 → .333 )
    - 打率が10割の場合は、「1.00」と出力すること。
    - 打率を計算しない場合は、「----」と出力すること。

--- tests/functional/ba_calcCept.php
+++ tests/functional/ba_calcCept.php
@@ -20,4 +20,4 @@ $I->seeInShellOutput("Usage");
 
 $I->expect("PA: 749, BA: 686, H:213 => BA: 0.310");
 $I->runShellCommmand("php app/ba_calc.php 749 686 213");
-$I->seeInShellOutput("Average: 0.310");
+$I->seeInShellOutput("Average: .310");
